297db7f5330f89fca423c2853793f390a9697ae1
Robin Seerig <robin@seerig.de>
2024-06-27 12:46:45 +0200

    real fix

--- components/core/Footer.tsx
+++ components/core/Footer.tsx
@@ -7,15 +7,15 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.left}>
-        <Linked type={MetadataTypes.instagram} href="https://www.instagram.com/singularity.2n40/" openInNewTab />
-        <Linked type={MetadataTypes.github} href="https://github.com/DRUMNICORN/2n40" openInNewTab />
+        <Linked type={MetadataTypes.instagram} href="https://www.instagram.com/singularity.2n40/" />
+        <Linked type={MetadataTypes.github} href="https://github.com/DRUMNICORN/2n40" />
       </div>
       <div className={styles.center}>
         {/* Center content, e.g., navigation links (optional) */}
         Made with ❤️
       </div>
       <div className={styles.right}>
-        <Linked textLeft label={'Impressum'} href='/impressum' type={MetadataTypes.info} />
+        <Linked label={'Impressum'} href='/impressum' type={MetadataTypes.info} />
       </div>
     </footer>
   );
